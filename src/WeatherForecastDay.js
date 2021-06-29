9a23147634ad5c85eff9a789407c34bbb9f5b951
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-06-29 13:43:31 +0200

    worked on weatherIcon, still not working

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -6,7 +6,7 @@ export default function WeatherForecastDay (props) {
     return (
         <div>
     <div className="WeatherForecastDay"> Thu </div>
-  <WeatherIcon  data= {props.data.weather[0].icon} size={50}/>
+ <WeatherIcon  icon= {props.data.weather[0].icon} size={50}/>
        <div className="ForecastTemp">
     <span>{props.data.temp.max}° </span>|
     <span> {props.data.temp.min}°</span>
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,9 +3,9 @@ import React from "react";
 
 
 export default function WeatherIcon(props) {
-  
+  let icon = props.icon;
   let weatherData = {
-    imgUrl: `https://openweathermap.org/img/wn/${props.data.icon}@2x.png`
+    imgUrl: `https://openweathermap.org/img/wn/${icon}@2x.png`
   };
   return <img className="WeatherIcon" src={weatherData.imgUrl} alt="/" width={props.size} />;
  
